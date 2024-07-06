d2b9039852ac13f9e4ca558bfe41f7a8fe6cfb8b
Hamed Samsami <samsami.hd@gmail.com>
2024-07-06 11:44:29 +0200

    feat: add about page and link to it on header

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,12 @@
 # myapp/urls.py
 from django.urls import path
 from .views import filtered_flood_events, index, detail
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', index, name='index'),
     path('filtered-flood-events/', filtered_flood_events,
          name='filtered_flood_events'),
     path('event/<int:pk>/', detail, name='detail'),
+    path('about/', TemplateView.as_view(template_name="core/about.html"), name='about'),
 ]
