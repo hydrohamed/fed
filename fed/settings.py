59a55663031e49b8dfd8ad9c560c6651b83b8ab1
Hamed Samsami <hydrohamed@gmail.com>
2024-07-08 11:04:48 +0200

    feat: add fed.hydrohamed.com to ALLOWED_HOSTS

--- fed/settings.py
+++ fed/settings.py
@@ -30,7 +30,8 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG"),
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "floodevents.onrender.com"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1",
+                 "floodevents.onrender.com", "fed.hydrohamed.com"]
 
 
 # Application definition
